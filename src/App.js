d49934f64cbc6934d4f59fab154f457d41765e3e
unknown <lateefahbaba123@gmail.com>
2024-01-24 15:28:34 +0530

    first progam

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import First from "./Components/First";
+let first= document.querySelector(".hii");
+// first.innerHTML="yesIt also work"
 const App =()=>{
  return(
     <div>
         
 <p>hi there</p>
 <h1>hey how are you</h1>
+<First
+name="apple"
+color="green"
+price="200"
+ />
     </div>
  )
 }
--- src/Components/First.js
+++ src/Components/First.js
@@ -1,8 +1,14 @@
 import React from "react";
 
-const First=()=>{
+const First=({name,color,price})=>{
     return(
-        <P>HI THERE</P>
+        <div><p className="hii">HI THERE{color}</p>
+        <p>my name {name}</p>
+        <p> price {price}</p>
+        <h1>this is my first as you can say progam in react i learn lot of things about
+            react but i did,not practice lot of things about react
+        </h1>
+  </div>      
     )
 }
 export default First
